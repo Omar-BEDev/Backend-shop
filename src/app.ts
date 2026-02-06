654781747e0a3b8b94f561c2c13af2518c7b9a39
Omar <kaliochibrahim@gmail.com>
2026-02-07 00:55:03 +0100

    Add CORS middleware to Express app

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,14 @@ import ExpressMongoSanitize from "express-mongo-sanitize"
 import orderRouter from "./api/routes/orders.route"
 import productRouter from "./api/routes/products.route"
 import userRouter from "./api/routes/users.route"
-
+import cors from "cors"
 const app = express()
 
 app.use(helmet())
 app.use(express.json())
 app.use(ExpressMongoSanitize())
 
-
+app.use(cors())
 app.use("/api/orders", orderRouter)
 app.use("/api/products", productRouter)
 app.use("/api/users",userRouter)
